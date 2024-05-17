750e3dfc0f321af8d39b6934c9a712144e56c065
srivatsa96 <saisrivatsa143@gmail.com>
2024-05-17 14:56:04 +0530

    new changes are added

--- app.py
+++ app.py
@@ -5,23 +5,27 @@ import streamlit as st
 import os
 
 load_dotenv()
-os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
+# os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY_PUBLIC')
 
 
-def get_openai_response(question):
+def get_openai_response(auth_token, question):
     # llm = Ollama(model= 'llama3-chatqa')
-    llm = ChatOpenAI(model='babbage-002')
-    return llm(question)
+    print("auth", auth_token)
+    # llm = ChatOpenAI(openai_api_key = auth_token, model='gpt-3.5-turbo-instruct')
+    llm = ChatOpenAI(openai_api_key=auth_token, model='gpt-3.5-turbo')
+    return llm.predict(question)
 
 #initialize streamlit app
 st.set_page_config(page_title="Q&A Demo")
-st.title("Langchain application")
+st.title("Langchain application1")
 
+key = st.text_input("Auth-token", key='password', type='password')
 input = st.text_input("Input:",key='input')
-response=get_openai_response(input)
+
+# response=get_openai_response(key, input)
 
 submit=st.button("Ask question")
 
-if submit:
+if submit and key:
     st.subheader("The response is:")
-    st.write(response)
+    st.write(get_openai_response(key, input))
